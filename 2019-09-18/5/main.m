a6f1d92ac2bd720bba283bc9f4710364ced9d86a
li-xie <lixie943@gmail.com>
2019-09-18 15:28:05 -0700

    B0=1e2 S=10

--- 2019-09-18/5/main.m
+++ 2019-09-18/5/main.m
@@ -6,7 +6,7 @@ for c = 1:3
     num_wells = 100;
     mu = 10^-3;
     label = num2str(c);
-    S = 0;
+    S = 10;
    
     simulateGroupSelectionFn(B0,prop,S,num_cycles,...
         num_wells,mu,label)
