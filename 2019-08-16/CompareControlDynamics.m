df0898a15c6b018059e966f3b797f8c07462da68
li-xie <lixie943@gmail.com>
2019-09-19 12:52:23 -0700

    tried different values of N to match the rate

--- 2019-08-16/CompareControlDynamics.m
+++ 2019-08-16/CompareControlDynamics.m
@@ -9,12 +9,12 @@ S = 100;
 T = 5.5;
 A0 = .13;
 
-N = B0*T;
+N = 470;%B0*T;
 Ub = mu*(1-A0)*2;
 sigma = A0 * sigQ;
-% v1 = sigma^2 * (log(2*N*Ub*log(sigma/Ub)))^2 /2 ...
-%     /log(sigma/Ub/sqrt(pi)*log(2*N*Ub)/sqrt(log(sigma/Ub)));
-v1 = sigma^2 * (log(N*Ub))^2 /2 /log(sigma/Ub);
+v1 = sigma^2 * (log(2*N*Ub*log(sigma/Ub)))^2 /2 ...
+    /log(sigma/Ub/sqrt(pi)*log(2*N*Ub)/sqrt(log(sigma/Ub)));
+% v1 = sigma^2 * (log(N*Ub))^2 /2 /log(sigma/Ub);
 v2 = (24 *mu^2 *(sigQ*A0)^4 *(1-A0)^2 *log(B0*T))^(1/3);
 figure(2)
 for c = 1:3
@@ -22,6 +22,6 @@ for c = 1:3
     plot((1:num_cycles), mean(fpT_all,2))
     hold on
 end
-plot((1:num_cycles), A0-(1:num_cycles)*1.5e-6*T)
+plot((1:num_cycles), A0-(1:num_cycles)*v1*T)
 % plot((1:num_cycles), A0-(1:num_cycles)*v2*T,':')
 hold off
