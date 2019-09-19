4abc69c756ad404df7c99e4aa6dc317a37f3d0b9
li-xie <lixie943@gmail.com>
2019-09-19 13:07:54 -0700

    collect info for number of genotypes

--- 2019-09-18/data_group.m
+++ 2019-09-18/data_group.m
@@ -17,18 +17,22 @@ for c = 1:3
     fp0_sel = zeros(num_cycles, max(S,1));
     B0_sel = zeros(num_cycles, max(S,1));
     varfp0_sel = zeros(num_cycles, max(S,1));
+    n_genos0_sel = zeros(num_cycles, max(S,1));
     
     fpT_sel = zeros(num_cycles, max(S,1));
     BT_sel = zeros(num_cycles, max(S,1));
     varfpT_sel = zeros(num_cycles, max(S,1));
+    n_genosT_sel = zeros(num_cycles, max(S,1));
     
     fp0_all = zeros(num_cycles, num_wells);
     B0_all = zeros(num_cycles, num_wells);
     varfp0_all = zeros(num_cycles, num_wells);
+    n_genos0_all = zeros(num_cycles, num_wells);
     
     fpT_all = zeros(num_cycles, num_wells);
     varfpT_all = zeros(num_cycles, num_wells);
     BT_all = zeros(num_cycles, num_wells);
+    n_genosT_all = zeros(num_cycles, num_wells);
     
     P_all = zeros(num_cycles, num_wells);
     P_sel = zeros(num_cycles, max(S,1));
@@ -44,9 +48,12 @@ for c = 1:3
             end
             fp0_sel(i, j) = sum(newb_fp{j}.*newb_b{j}.*newb_n{j})/sum(newb_b{j}.*newb_n{j});
             varfp0_sel(i, j) = sum((newb_fp{j}-fp0_sel(i, j)).^2.*newb_b{j}.*newb_n{j})/sum(newb_b{j}.*newb_n{j});
+            n_genos0_sel(i, j) = newb_n_genos{j};
+            
             BT_sel(i, j) = sum(b{j}.*n{j});
             fpT_sel(i, j) = sum(fp{j}.*b{j}.*n{j})/sum(b{j}.*n{j});
             varfpT_sel(i, j) = sum((fp{j}-fpT_sel(i, j)).^2.*b{j}.*n{j})/sum(b{j}.*n{j});
+            n_genosT_sel(i, j) = n_genos{j};
             P_sel(i, j) = P{j}(end);
         end
         
@@ -59,14 +66,18 @@ for c = 1:3
             end
             fp0_all(i, j) = sum(newb_fp{j}.*newb_b{j}.*newb_n{j})/sum(newb_b{j}.*newb_n{j});
             varfp0_all(i, j) = sum((newb_fp{j}-fp0_all(i, j)).^2.*newb_b{j}.*newb_n{j})/sum(newb_b{j}.*newb_n{j});
+            n_genos0_all(i, j) = newb_n_genos{j};
+            
             BT_all(i, j) = sum(b{j}.*n{j});
             fpT_all(i, j) = sum(fp{j}.*b{j}.*n{j})/sum(b{j}.*n{j});
             varfpT_all(i, j) = sum((fp{j}-fpT_all(i, j)).^2.*b{j}.*n{j})/sum(b{j}.*n{j});
+            n_genosT_all(i, j) = n_genos{j};
             P_all(i, j) = P{j}(end);
         end
         
     end
     save([num2str(f) '/' 'fp_data' num2str(c)],'fp0_sel','varfp0_sel','B0_sel',...
         'fpT_sel','varfpT_sel','BT_sel','fp0_all','varfp0_all','B0_all',...
-        'fpT_all','varfpT_all','BT_all','P_all','P_sel')
+        'fpT_all','varfpT_all','BT_all','P_all','P_sel',...
+        'n_genos0_sel','n_genosT_sel','n_genos0_all','n_genosT_all')
 end
