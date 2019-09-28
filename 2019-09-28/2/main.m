837d657e03c9a4bccf8f6e96f8336383fcc3d5e0
li-xie <lixie943@gmail.com>
2019-09-28 15:56:18 -0700

    copied from 1, B0=1e4 S=10

---
+++ 2019-09-28/2/main.m
@@ -0,0 +1,13 @@
+clear
+for c = 1:3
+    B0 = 1e4;
+    prop = @pipette;
+    num_cycles = 1e3;
+    num_wells = 100;
+    mu = 10^-3;
+    label = num2str(c);
+    S = 10;
+   
+    simulateGroupSelectionFn(B0,prop,S,num_cycles,...
+        num_wells,mu,label)
+end
