4f793202579801e178db9c15c48fb56063189856
li-xie <lixie943@gmail.com>
2019-10-22 18:13:22 -0700

    save cycle number and seed

--- 2019-10-16/1/save_newborns.m
+++ 2019-10-16/1/save_newborns.m
@@ -1,11 +1,11 @@
 function save_newborns(newb_fp, newb_b, newb_n, newb_n_genos,...
-    newb_pnum, seed, gen, resultsfolder)
+    newb_pnum, seed, cycle_num, resultsfolder)
 nb.newb_fp = newb_fp;
 nb.newb_b = newb_b;
 nb.newb_n = newb_n;
 nb.newb_n_genos = newb_n_genos;
-nb.gen = gen + 1;
+nb.cycle = cycle_num;
 nb.newb_pnum = newb_pnum;
 nb.seed = seed;
-save(strcat(resultsfolder,'nb',num2str(gen + 1)),'-struct','nb');
+save(strcat(resultsfolder,'nb',num2str(cycle_num)),'-struct','nb');
 end
