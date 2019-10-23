f58f0588c88302c6f17141486a5b7705650fcff0
li-xie <lixie943@gmail.com>
2019-10-22 17:54:34 -0700

    modify to save the seed

--- 2019-10-16/1/save_newborns.m
+++ 2019-10-16/1/save_newborns.m
@@ -1,10 +1,11 @@
-function save_newborns(newb_fp,newb_b,newb_n,newb_n_genos,...
-    newb_pnum, gen, resultsfolder)
+function save_newborns(newb_fp, newb_b, newb_n, newb_n_genos,...
+    newb_pnum, seed, gen, resultsfolder)
 nb.newb_fp = newb_fp;
 nb.newb_b = newb_b;
 nb.newb_n = newb_n;
 nb.newb_n_genos = newb_n_genos;
 nb.gen = gen + 1;
-nb.pnum = newb_pnum;
+nb.newb_pnum = newb_pnum;
+nb.seed = seed;
 save(strcat(resultsfolder,'nb',num2str(gen + 1)),'-struct','nb');
 end
